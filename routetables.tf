012d945bf4fa46abe22013a2b79f777e3158838b
Nitish Hanchinagoudra <nithanch@publicisgroupe.net>
2020-08-07 18:22:16 +0530

    added missing route table entry for the private subnet

--- routetables.tf
+++ routetables.tf
@@ -16,6 +16,10 @@ resource "aws_route_table_association" "public_rt" {
 }
 resource "aws_route_table" "private_rt" {
   vpc_id = aws_vpc.equalexperts.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.natgw.id
+  }
   tags = {
     "Name"  = "ee_private_rt"
     "Owner" = "equal_experts"
